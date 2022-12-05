acb7d3231873ce2e34e5989aa2c98e2e36121cc8
Tanyasahapova2009 <tanyasahapova@mail.ru>
2022-12-05 23:15:04 +0300

    313

---
+++ zd313/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите двух- или трехзначное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 100;
+int c = a % 10;
+int d = b % 10;
+if (d>0)
+Console.WriteLine($"третья цифра {c}");
+else Console.WriteLine("третей цифры нет");
