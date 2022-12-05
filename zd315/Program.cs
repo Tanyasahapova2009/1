eba04a3fbb853c3573b973f87e31ab2cb85f727d
Tanyasahapova2009 <tanyasahapova@mail.ru>
2022-12-05 23:20:06 +0300

    315

---
+++ zd315/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите цифру обозначающую день недели ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7 )
+Console.WriteLine("Это выходной день");
+else Console.WriteLine("Это будний день");
+
